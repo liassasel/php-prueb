690f663ac9c3237312f578ab9f21a30e9852fee6
Angel <auseche80@gmail.com>
2024-10-11 13:30:57 -0400

    1.1 Se utiliza cURL

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+La URL debe pasarse como parámetro 'url' a través de la barra de direcciones del navegador 
+Ejempo: http://localhost/evento-analisis/index.php?url=https://www.vividseats.com/... 
+*/
+
 //Verifica si se le ha pasado la URl como parámetro 
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
@@ -32,6 +37,14 @@ if ($platform == 'Desconocida') {
 
 // Función para obtener el contenido HTML de la URL
 
+/*
+
+Al utilizar este método sale el error '403 Forbidden' cual sale por que se esta trabajando con file_get_contents
+y el servidor esta bloqueando la solicitud ya que se esta realizando la peticion desde un script en lugar de un navegador web
+para solucionar la misma se implementara una alternativa con cURL que simulara la petición de un navegador, lo que nos permitirá 
+obtener el contenido HTML de la página sin bloqueos.
+
+
 function getHtml($url) {
     // Se intenta obtener el HTML de la página
     $html = file_get_contents($url);
@@ -44,8 +57,42 @@ function getHtml($url) {
     return $html; //Devuelve el contenido del HTML
 
 }
+    */
 
-$html = getHtml($url); // HTML de la página
+function getHtml($url) {
+    //Se inicializa cURL
+
+    $ch = curl_init();
+
+    //Se establecen las opciones del cURl
+    curl_setopt($ch, CURLOPT_URL, $url); // URL a la que se le hará la solicitud 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Regresa el resultado comop un String
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Seguir redirecciones, si las hay
+
+    // Se simula un navegador estableciendo User-Agent
+
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36');
+
+    // Ejecuta la solicitud
+    $html = curl_exec($ch);
+
+    // Verificación de errores
+    if (curl_exec($ch)) {
+        echo "Error: No se pudo obtener el contenido de la página. " . curl_error($ch);
+        curl_close($ch);
+        exit();
+    }
+
+    // Cierra la conexión con cURL
+    curl_close($ch);
+
+    return $html; // Regresa el html de la página para finalizar 
+}
+
+
+
+$html = getHtml($url); // HTML de la página usando  cURL
+
 
 // Función para procesar el HTML de una página de VividSeats y extraer sus datos (Entradas disponibles, fila y precio)
 
@@ -63,7 +110,7 @@ function  processVividSeats($html) {
 
 // Aqui se debe analizar el HTML de la página VividSeats para encontrar elementos de la tablas que tiene los sectores, precios y filas
 
-$tickets = $xpath->query('//*[@id="start-of-content"]/main/div[1]/div[2]/div[2]/section[2]/div[3]/ul/li');
+$tickets = $xpath->query('//*[@id="__next"]/div[3]/div[2]/div[2]/div[2]/div[1]/div/header');
 
 if ($tickets->length > 0) {
     // Itera los resultados y se extrae la información
